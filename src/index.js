cbd278c9243b6802cee875127bb78b2cb17e9fd4
Ajit-Dwivedi <dwivediajit203@gmail.com>
2024-09-25 11:39:57 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import RouteComponent from "./Route";
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouteComponent />);
+//root.render(<App />);
+
